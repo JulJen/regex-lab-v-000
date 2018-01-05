3941fa3dd8a91838e738b3a09a8ff3b940e78272
Julia J-Prog <juliajen.prog@gmail.com>
2018-01-05 04:00:36 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,23 @@
 def starts_with_a_vowel?(word)
-
+  !!(word[0].match(/[aeiou]/i))
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/[un][a-z]*ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b[a-z]{5}\b/i)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  !!text.match(/^[A-Z].*[\.,:;]$/)
 end
 
 def valid_phone_number?(phone)
-
+    !!phone.match(/\b\d[0-9]*{10,}\b/)
+  # !!phone.match(/\b[0-9]*\w{3}\b/)
+  # !!phone.match(/\b(\d[0-9]*{10})\b/)
+  # !!phone.match(/(\d{10}*[0-9])/)
+  # !!phone.match(/( *?[0-9a-zA-Z] *?){10,}/)
 end
